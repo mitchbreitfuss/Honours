51198e34552d059810ad179b84af55bd1ac84951
Mitchell Breitfuss <mitchell.breitfuss@gmail.com>
2019-04-23 11:05:14 +1000

    Moved functions to header file

--- switching/switching.ino
+++ switching/switching.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
-
+#include "header.h"
 
 const char* ssid1     = "Breitfuss";
 const char* password1 = "mitchell";
@@ -23,7 +23,7 @@ SoftwareSerial COM(D5,D6);
 
 
 String file;
-bool isHTML;
+
 
 String stringType = "type=string";
 String dataType = "type=float";
@@ -52,127 +52,14 @@ void setup()
 
 }
 
-void getFile(String fileName, String fileType, String hostAdd){  
-    int value;
-    Serial.print("Connecting to ");
-    Serial.println(hostAdd);
-
-    // Use WiFiClient class to create TCP connections
-    WiFiClient client;
-    const int httpPort = 8000;
-
-    if (!client.connect(hostAdd, httpPort)) {
-        Serial.println("connection failed");
-    return;
-    }
-    client.println(String("GET /") + fileName + " HTTP/1.1");
-
-    // We now create a URI for the request
-    String url = "/input/";
-    url += streamId;
-    url += "?private_key=";
-    url += privateKey;
-    url += "&value=";
-    url += value;
-
-    Serial.print("Requesting URL: ");
-    Serial.println(url);
-
-    // This will send the request to the server
-    client.print(String("GET /")+ fileName + url + " HTTP/1.1\r\n" +
-                "Host: " + hostAdd + "\r\n" + 
-                "Connection: close\r\n\r\n");
-    Serial.println(String("Requesting ") + fileName);
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-        Serial.println(">>> Client Timeout !");
-        client.stop();
-        return;
-    }
-    }
-
-    // Read all the lines of the reply from server and print them to Serial
-    while(client.available()){
-    delay(10);
-    String line = client.readStringUntil('\n');
-
-    if(fileType == "html-data"){
-        if(line.indexOf("<html>")> -1){
-
-            isHTML = true;
-            Serial.println("html-start");
-            
-        }
-
-        if(isHTML == true){
-            
-            Serial.print(line);
-            COM.print("[HTML] " + line + "$M\n");
-        }
-        if(line.indexOf("</html>")>-1){
-            isHTML = false;
-            //Serial.println("html-start");
-            //Serial.print(file);
-            Serial.println();
-            Serial.println("html-stop");
-            client.stop();
-        }
-        
-        
-        
-    }
-        if(fileType == "config"){
-        if(line.indexOf("option1")>-1){
-            
-            line.replace("option1 = ","");
-            option1 = line;
-        }
-
-        if(line.indexOf("option2")>-1){
-        
-            line.replace("option2 = ","");
-            option2 = line;
-        }
-
-        if(line.indexOf("option3")>-1){
-            Serial.println("WORKING");
-            line.replace("option3 = ","");
-            option3 = line;
-        }
-    }
-
-    }
-    Serial.println("closing connection");
-    //if(fileType == "config"){
-    //Serial.println(option1);
-    //Serial.println(option2);
-    //Serial.println(option3);
-   // }
-}
-
-
-void send(float toSend){
-    COM.print(String(toSend) + "$D");
-  
-    Serial.println(toSend);
-}
-
-void send(String toSend){
-   
-    COM.print(toSend+"$M");
-    
-    Serial.print("Sending message: ");
-    Serial.println(toSend);
-}
 
 int server = 0;
 float toSend = 3.14;
 void loop(){
 
 
-    if(COM.available()){
-        String recieved = COM.readStringUntil('\n');
+    if(Serial.available()){
+        String recieved = Serial.readStringUntil('\n');
         
         if(recieved.indexOf("$C")>0){
             recieved.replace("$C","");
@@ -197,11 +84,12 @@ void loop(){
 
     delay(100);
     toSend = toSend + 0.01;
-    COM.print(String(toSend)+"$D\n");
+    Serial.print(String(toSend)+"$D\n");
     
 
     switch (server){
         case 1:
+            // This corresponds to the inverter.
             WiFi.begin(ssid1,password1);
 
             while(WiFi.status() != WL_CONNECTED){
@@ -213,7 +101,7 @@ void loop(){
             Serial.println("Connected to AP 1");
             Serial.println("Wifi Connected\n IP Address: " + WiFi.localIP());
             Serial.println("Success! Connected to server " + server);
-            getFile("elweb.html","html-data","10.0.0.100");
+            getFile("Monitor%20Page.html","html-data","10.0.0.100",server);
 
             server = 0;
             WiFi.disconnect();
