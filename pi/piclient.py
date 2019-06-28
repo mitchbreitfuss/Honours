2cd88d127916974e5d65eb67026863fd136bf749
Mitch Breitfuss <mitchell.breitfuss@gmail.com>
2019-06-28 14:05:48 +1000

    Restructured

--- pi/piclient.py
+++ pi/piclient.py
@@ -35,149 +35,171 @@ while startup == 0:
 
 # Frontend User program class.
 # This class allows us to create a GUI application using tkinter.
-class Application(Frame):
-
-    AUTO_MODE = True
-
-    # This method is responsible for communicating over the serial port with the arduino.
+def readSerial():
+        # This method is responsible for communicating over the serial port with the arduino.
     # It also handles the data based on what kind of communication it is.
     # Currently there are 3 dataTypes, $D (data), $M (message), $C (command).
     # Data is for numbers relating to sensor readings or other quantitative data.
     # Messages are for general communications between the two devices, for debugging purposes.
     # Commands are for changing parameters on either device to change how it operates.
-    
-    
-    def readSerial(self):
-        x = ser.readline()  # Reads in a line from the serial port.
+    x = ser.readline()  # Reads in a line from the serial port.
 
-        # This if statement will only run if the read line x has non-zero length.
-        if(len(x) != 0):
-            # Converts from byte data to a utf-8 formatted string.
-            while True:
-                try:
-                    decodedMessage = x.decode('utf-8')
-                    break
-                except UnicodeDecodeError:
-                    decodedMessage = ''
-                    #print("[Error] Decode Error from serial")
-                    break
-            if("\n" in decodedMessage):
-                decodedMessage = decodedMessage.replace("\n", "")
+    # This if statement will only run if the read line x has non-zero length.
+    if(len(x) != 0):
+        # Converts from byte data to a utf-8 formatted string.
+        while True:
+            try:
+                decodedMessage = x.decode('utf-8')
+                break
+            except UnicodeDecodeError:
+                decodedMessage = ''
+                #print("[Error] Decode Error from serial")
+                break
+        if("\n" in decodedMessage):
+            decodedMessage = decodedMessage.replace("\n", "")
 
-            dataType = 0
+        dataType = 0
 
-            # The following block of code sorts the incoming message based on it's purpose.
-            # All aspects of this project utilise the same syntax of using a $ sign followed by a letter to denote a communication's purpose.
-            if("$D" in decodedMessage):
-                decodedMessage = decodedMessage.replace("$D", "")
-                dataType = "data"
-            if("$M" in decodedMessage):
-                decodedMessage = decodedMessage.replace("$M", "")
-                dataType = "message"
-            if("$C" in decodedMessage):
-                decodedMessage = decodedMessage.replace("$C", "")
-                dataType = "command"
-            if("$L" in decodedMessage):
-                decodedMessage = decodedMessage.replace("$L", "")
-                dataType = "log"
+        # The following block of code sorts the incoming message based on it's purpose.
+        # All aspects of this project utilise the same syntax of using a $ sign followed by a letter to denote a communication's purpose.
+        if("$D" in decodedMessage):
+            decodedMessage = decodedMessage.replace("$D", "")
+            dataType = "data"
+        if("$M" in decodedMessage):
+            decodedMessage = decodedMessage.replace("$M", "")
+            dataType = "message"
+        if("$C" in decodedMessage):
+            decodedMessage = decodedMessage.replace("$C", "")
+            dataType = "command"
+        if("$L" in decodedMessage):
+            decodedMessage = decodedMessage.replace("$L", "")
+            dataType = "log"
 
-            if(decodedMessage != ''):
-                if(dataType == "log"):
-                    self.displayMessage.delete(1.0, END)
-                    self.displayMessage.insert(END, decodedMessage)
-                    print("[Message] " + decodedMessage)
-                    outfile = open("log.txt", "a")
-                    outfile.write(decodedMessage + "    ")
-                if(dataType == "message"):
-                    self.displayMessage.delete(1.0, END)
-                    self.displayMessage.insert(END, decodedMessage)
-                    print("[Message] " + decodedMessage)
+        if(decodedMessage != ''):
+            if(dataType == "log"):
+                self.displayMessage.delete(1.0, END)
+                self.displayMessage.insert(END, decodedMessage)
+                print("[Message] " + decodedMessage)
+                outfile = open("log.txt", "a")
+                outfile.write(decodedMessage + "    ")
+            if(dataType == "message"):
+                self.displayMessage.delete(1.0, END)
+                self.displayMessage.insert(END, decodedMessage)
+                print("[Message] " + decodedMessage)
 
-                if(dataType == "data"):
-                    self.displayData.delete(1.0, END)
-                    self.displayData.insert(END, decodedMessage)
-                    print("[Data] " + decodedMessage)
-                    outfile = open("log.txt", "a")
-                    outfile.write(decodedMessage + "\n")
-                    outfile.close()
+            if(dataType == "data"):
+                self.displayData.delete(1.0, END)
+                self.displayData.insert(END, decodedMessage)
+                print("[Data] " + decodedMessage)
+                outfile = open("log.txt", "a")
+                outfile.write(decodedMessage + "\n")
+                outfile.close()
 
-        self.after(refreshRate, self.readSerial)
-    # Method for sending a command from the text box to the arduino
-    # Works in conjunction with retrieveInput.
+    self.after(refreshRate, self.readSerial)
 
-    def sendCommand(self, command=None):
 
-        if command == None:
-            command = self.retrieveInput()
-        if("\n" in command):
-            command = command.replace("\n", "")
-        print("Sending Command: ")
-        if("$S" not in command):
-            command = command + '$C'
-        print(command)
-        ser.write(bytes(command, "utf-8"))
 
-    # Method responsible for getting user input from the commandBox tkinter widget.
-    def retrieveInput(self):
-        input = self.commandBox.get(1.0, END)
-        self.commandBox.delete(1.0, END)
-        return input
 
-    # Placeholder method for testing connections to multiple wireless access points.
-    def serverConnect1(self):
-        print("Server 1")
-        command = "1$S\n"
-        self.sendCommand(command)
+def sendCommand(_event=None,command=None):
+# Method for sending a command from the text box to the arduino
+# Works in conjunction with retrieveInput.
+    if command == None:
+        command = retrieveInput()
+    if("\n" in command):
+        command = command.replace("\n", "")
+    print("Sending Command: ")
+    if("$S" not in command):
+        command = command + '$C'
+    print(command)
+    ser.write(bytes(command, "utf-8"))
 
-    # Placeholder method for testing connections to multiple wireless access points.
-    def serverConnect2(self):
-        print("Server 2")
-        command = "2$S\n"
-        self.sendCommand(command)
+# Method responsible for getting user input from the commandBox tkinter widget.
+def retrieveInput():
+    input = app.commandBox.get(1.0, END)
+    app.commandBox.delete(1.0, END)
+    return input
+
+# Placeholder method for testing connections to multiple wireless access points.
+def serverConnect1():
+    print("Server 1")
+    command = "1$S\n"
+    sendCommand(command)
+
+# Placeholder method for testing connections to multiple wireless access points.
+def serverConnect2():
+    print("Server 2")
+    command = "2$S\n"
+    sendCommand(command)
+
+# Function to recieve commands when running the program in headless mode. (WIP)
+def CLI():
+    while True:
+        input = input("Enter command to run: ")
+# Insert a command dictionary here to allow running commands from the commandline.
+# Considder using a shared header file between the Pi and Arduinos so when new functionality is added it is simpler to impliment.
+
+
+
+
+# GUI Object, using Tkinter.
+class Application(Frame):
+
+    AUTO_MODE = True
+
+        # Initialise function, runs at the startup of the GUI.
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+
+
 
     # This method is responsible for creating all of the GUI through tkinter.
     # This includes the GUI functionality.
     def createWidgets(self):
-
-          
-          
-##
-        #self.main_window = Frame(self,height=480,width=640,bg='white')
-        #self.main_window.grid(row=0,sticky=N+S+E+W)
-
+        # Below the GUI elements are created  
         
-
-        
-        self.ser1Button = Button(self,text="Server 1",command=self.serverConnect1)
+        # Server 1 Connect Button  
+        self.ser1Button = Button(self,text="Server 1",command=serverConnect1)
         self.ser1Button.grid(row=0,column=0)
-
-        self.ser2Button = Button(self,text="Server 2",command=self.serverConnect2)
+        
+        # Server 2 Connect Button
+        self.ser2Button = Button(self,text="Server 2",command=serverConnect2)
         self.ser2Button.grid(row=1,column=0)
 
+        # Message Box Label
         self.messageLabel = Label(self,text="Message")
         self.messageLabel.grid(row=0,column=2)
 
+        # Message Text Box
         self.displayMessage = Text(self,height=1,width=20)
         self.displayMessage.grid(row=0,column=3)
 
+        # Data Box Label
         self.dataLabel = Label(self,text="Data")
         self.dataLabel.grid(row=1,column=2)
 
+        # Data Text Box
         self.displayData = Text(self,height=1,width=20)
         self.displayData.grid(row=1,column=3)
 
+        # Quit Button
         self.QUIT = Button(self,text = "QUIT",command=self.quit)
         self.QUIT.grid(row=0,column=4)
 
+        # Command Box Label
         self.commandLabel = Label(self,text="Command")
         self.commandLabel.grid(row=6,column=2)
 
+        # Command Text Box
         self.commandBox=Text(self,height=1,width=20)
         self.commandBox.grid(row=6,column=3)
 
-        self.commandButton = Button(self,text="Send",command=self.sendCommand)
+        # Command Send Button
+        self.commandButton = Button(self,text="Send",command=sendCommand)
         self.commandButton.grid(row=6,column=4)
-         
+        self.master.bind("<Return>",sendCommand) # Bind return key to send the command for better UX
+
        #Old pack based GUI code
 ##        self.QUIT.pack({"side": "right"})
 ##
@@ -209,23 +231,19 @@ class Application(Frame):
 ##        self.commandButton["command"] = self.sendCommand
 ##        self.commandButton.pack()
 
-    # Initialise function, runs at the startup of the GUI.
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.grid()
-        self.createWidgets()
-        
-        
-        #self.readSerial()
-        #self.pack()
 
 
+
+
+
+# Initialise tkinter.
 master = Tk()
 
-
+# Set Application object to variable app.
 app = Application()
 
-
+# Runs the application loop. (Tkinter)
 app.mainloop()
 
+# Garbage collection for when the mainloop is stopped.
 master.destroy()
